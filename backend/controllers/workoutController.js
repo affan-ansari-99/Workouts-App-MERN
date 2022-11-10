dfee6bd2e971a041de0714e28bc5e87e1b8fc692
Affan Arif <muhammad.affan@devsinc.com>
2022-11-10 17:10:32 +0500

    Create workout function
    * Adds a workout record to DB

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -5,7 +5,20 @@ const Workout = require("../models/workoutModel");
 // get a single workouts
 
 // create new  workout
-
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+  // add doc to db
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 // delete a workout
 
 // update a workout
+
+module.exports = {
+  createWorkout: createWorkout,
+};
