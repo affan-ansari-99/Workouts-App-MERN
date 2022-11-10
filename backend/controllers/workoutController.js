858eb6e5221d2675d9f800e39c7eaf7e4e92c1d0
Affan Arif <muhammad.affan@devsinc.com>
2022-11-10 18:01:07 +0500

    updateWorkout function
    * Updates a single workout from DB

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -53,11 +53,28 @@ const deleteWorkout = async (req, res) => {
 
   return res.status(200).json(workout);
 };
+
 // update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  return res.status(200).json(workout);
+};
 
 module.exports = {
   createWorkout,
   getWorkouts,
   getWorkout,
   deleteWorkout,
+  updateWorkout,
 };
